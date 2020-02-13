abc428b0e98a74e05a39407f7096f6e3133ac4ec
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-02-13 22:43:31 +0800

    clash图形简易版

---
+++ luci-app-openclash/files/usr/share/openclash/clash_version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CKTIME=$(date "+%Y-%m-%d-%H")
+LAST_OPVER="/tmp/clash_last_version"
+version_url="https://raw.githubusercontent.com/vernesong/OpenClash/master/core_version"
+if [ "$CKTIME" != "$(grep "CheckTime" $LAST_OPVER 2>/dev/null |awk -F ':' '{print $2}')" ]; then
+   curl -sL --connect-timeout 10 --retry 2 "$version_url" -o $LAST_OPVER >/dev/null 2>&1
+   if [ "$?" -eq "0" ] && [ "$(ls -l $LAST_OPVER 2>/dev/null |awk '{print int($5)}')" -gt 0 ]; then
+      echo "CheckTime:$CKTIME" >>$LAST_OPVER
+   else
+      rm -rf $LAST_OPVER
+   fi
+fi
